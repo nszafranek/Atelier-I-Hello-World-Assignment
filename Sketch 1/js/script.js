765712a468468e27d73baf04fc9ea5a1c167a799
nszafranek <nik.szafranek@gmail.com>
2018-09-13 10:18:53 -0400

    Feexed

--- Sketch 1/js/script.js
+++ Sketch 1/js/script.js
@@ -1,8 +1,11 @@
+var i
+
 function setup() {
-  createCanvas(720,480)
+  createCanvas(720,480);
 }
 function draw() {
   for(var i = 0; i < 10; i++) {
-    ellipse(10,10)
+    ellipse(10*i+i,10*i+i,10+i);
   }
 }
++i+i+i
--- Sketch 2/js/script.js
+++ Sketch 2/js/script.js
@@ -1,6 +1,8 @@
 function setup() {
-  createCanvas(720,480)
+  createCanvas(720,480);
 }
 function draw() {
-
+  for(var i = 0; i < 10; i++) {
+    ellipse(10,10,10);
+  }
 }
