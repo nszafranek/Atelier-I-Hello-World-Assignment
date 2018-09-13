1cdf2816b9ec114650e8a05a192b02510a454d00
nszafranek <nik.szafranek@gmail.com>
2018-09-13 11:51:29 -0400

    Sketch3 things

--- Sketch 1/js/script.js
+++ Sketch 1/js/script.js
@@ -1,10 +1,26 @@
-var i
+var mouseClicks = [];
 
 function setup() {
   createCanvas(720,480);
 }
+
 function draw() {
-  for(var i = 0; i < 10; i++) {
-    ellipse(10*i+i,10*i+i,10+i);
+  for (var i = 0; i < mouseClicks.length; i++) {
+    ellipse(mouseClicks[i].x, mouseClicks[i].y, 10);
+    mouseClicks[i].x = mouseClicks[i].x + mouseClicks[i].speed;
+    mouseClicks[i].y = mouseClicks[i].x + mouseClicks[i].speed;
   }
+}
+
+function mouseClicked() {
+  var clickPosition = {
+    x: mouseX,
+    y: mouseY,
+    speed: random(-2, 2)
+  }
+  mouseClicks.push(clickPosition);
+}
+
+function clickPosition() {
+
 }
